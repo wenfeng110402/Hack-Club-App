163e564390e0dbe2843864f3da3e47fc02f3948f
WenfengYe <cret@cret.uk>
2026-04-10 20:43:38 +0800

    Initial Commit

---
+++ HackClubApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HackClubApp
+//
+//  Created by 叶文峰 on 2026/4/10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HackClubApp/HackClubAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackClubAppApp.swift
+//  HackClubApp
+//
+//  Created by 叶文峰 on 2026/4/10.
+//
+
+import SwiftUI
+
+@main
+struct HackClubAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
